36bde9fa447e192a6d0806176007de1349f03d84
Simon Hudon <simon.hudon@gmail.com>
2022-01-31 13:57:07 -0500

    feat: print_vars! can now use monadic pretty printing

---
+++ lib/lib/Meta/Dump.lean
@@ -0,0 +1,92 @@
+import Lean.PrettyPrinter
+
+import Lib.Meta.Format
+import Lib.Data.List.Control
+
+namespace Lean.Syntax
+variable [Monad m]
+variable [MonadRef m]
+variable [MonadQuotation m]
+
+def mkListLit : List Syntax → m Syntax
+| [] => `(List.nil)
+| x :: xs => do `(List.cons $x $(← mkListLit xs))
+
+end Lean.Syntax
+
+namespace Lean.Elab.Tactic
+
+open Syntax Lean.Meta
+
+def Syntax.mkStringLit' (ref : Syntax) (s : String) : Syntax :=
+let pos := ref.getHeadInfo
+node pos strLitKind #[atom pos s]
+
+def Syntax.mkStringLit [Monad m] [MonadRef m] (s : String) : m Syntax := do
+let pos ← MonadRef.mkInfoFromRefPos
+return node pos strLitKind #[atom pos s]
+
+def padding (margin : Nat) (s : String) : String :=
+s ++ Nat.repeat (" " ++ .) (margin - s.length) ""
+
+section printVars
+variable [MonadLiftT IO m] [Monad m]
+
+@[inline]
+def printVarsAux (acc : Format) :
+  List (String × m Format) → m Unit
+| [] => IO.println acc
+| (x, y) :: [] => do
+  let y ← y
+  let acc := acc ++ x ++ y
+  IO.println acc
+| (x, y) :: xs => do
+  let y ← y
+  printVarsAux (acc ++ x ++ y ++ format "\n") xs
+
+@[inline]
+def printVars [MonadLiftT IO m] [Monad m] :
+  List (String × m Format) → m Unit :=
+printVarsAux ""
+
+end printVars
+
+def dumpListAux : List Syntax → TermElabM Syntax
+| [] => `( "" )
+| [x] => pure x
+| x :: xs => do
+  `( $x ++ "\n" ++ $(← dumpListAux xs) )
+
+def dumpList (ls : List Syntax) : TermElabM Expr := do
+  let out ← ls.mapM (λ x => do
+    toString (← Lean.PrettyPrinter.ppTerm x))
+  let len ← out.map String.length |>.maximum? |>.get!
+  let out := out.map <| (padding len . ++ " = ")
+  let lines ← out.zipWithM ls λ x y =>
+    `($(Syntax.mkStrLit x) ++ toString $y)
+  Lean.Elab.Term.elabTerm
+    (← dumpListAux lines)
+    (some (mkConst ``String))
+
+def dumpListMonadic (ls : List Syntax) : TermElabM Expr := do
+  let out ← ls.mapM (λ x => do
+    toString (← Lean.PrettyPrinter.ppTerm x))
+  let len ← out.map String.length |>.maximum? |>.get!
+  let out := out.map <| (padding len . ++ " = ")
+  let lines ← out.zipWithM ls λ x y =>
+    `(($(Syntax.mkStrLit x), Std.ToFormatM.formatM $y))
+  let lines ← mkListLit lines
+  Lean.Elab.Term.elabTerm
+    (← ``(printVars $lines))
+    none
+
+elab "dump_list!" "[" t:(term,*) "]" : term =>
+  dumpList t.getSepArgs.toList
+
+elab "print_vars!" "[" t:(term,*) "]" : term => do
+  dumpListMonadic t.getSepArgs.toList
+
+elab "dump!" t:term : term =>
+  dumpList [t]
+
+end Lean.Elab.Tactic
---
+++ lib/lib/Meta/Format.lean
@@ -0,0 +1,83 @@
+
+import Lib.Meta.DeclGraph
+
+namespace Std
+open Lean
+class ToFormatM (m : Type → Type v) (α : Type u) where
+  formatM : α → m Format
+  modifier : List Name := []
+
+open ToFormatM
+
+instance [Monad m] [ToFormatM m α] : ToFormatM m (List α) where
+  formatM x := do format (← x.mapM formatM)
+
+instance [Monad m] [ToFormatM m α] : ToFormatM m (Subarray α) where
+  formatM x := do format (← x.toArray.mapM formatM)
+
+instance [Monad m] [ToFormatM m α] : ToFormatM m (Array α) where
+  formatM x := do format (← x.mapM formatM)
+
+instance [MonadLiftT m n] [ToFormatM m α] : ToFormatM n α where
+  formatM x := liftM (formatM x : m _)
+  modifier := ToFormatM.modifier m α
+
+instance (priority := low)
+         [Pure m] [ToFormat α] : ToFormatM m α where
+  formatM x := pure <| format x
+
+end Std
+
+namespace Lean
+open Std ToFormatM Meta
+open Lean.Elab.Tactic
+
+instance : ToFormatM MetaM Expr where
+  formatM := Meta.ppExpr
+
+instance : ToFormatM MetaM FVarId where
+  formatM v := Meta.ppExpr <| mkFVar v
+
+def WithType := Expr
+@[inline] def withType : Expr → WithType := id
+
+def AsGoal := MVarId
+@[inline] def asGoal : MVarId → AsGoal := id
+
+def MainGoal := Unit
+@[inline] def mainGoal : MainGoal := ()
+
+def GoalList := Unit
+@[inline] def goalList : GoalList := ()
+
+instance : ToFormatM MetaM WithType where
+  formatM x := do
+    let t ← Meta.ppExpr (← Meta.inferType x)
+    let x ← Meta.ppExpr x
+    return x ++ " : " ++ t
+  modifier := [`withType]
+
+instance : ToFormatM MetaM AsGoal where
+  formatM x := Format.indentD <$> Meta.ppGoal x
+  modifier := [`asGoal]
+
+instance : ToFormatM TacticM MainGoal where
+  formatM x := do
+    let g ← getMainGoal
+    Format.indentD <$> liftMetaM (Meta.ppGoal g)
+
+instance : ToFormatM TacticM GoalList where
+  formatM x := do
+    let gs ← getGoals
+    let mut gs ← gs.mapM λ g =>
+       liftMetaM (Meta.ppGoal g)
+    if gs.length = 0 then
+      gs := ["No goals"]
+    else if gs.length > 1 then
+      gs := s!"{gs.length} Goals" :: gs
+    return Format.joinSep (gs.map Format.indentD) Format.line
+
+instance [ToFormatM MetaM α] : ToFormatM TacticM α where
+  formatM x := Meta.liftMetaM <| formatM x
+
+end Lean
