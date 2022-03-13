30789d51c90f64f5751d1d7da401ef2760afe6d9
Simon Hudon <simon.hudon@gmail.com>
2022-03-13 16:43:35 -0400

    feat: decidable subtypes `DecSubtype`

---
+++ lib/lib/Data/DecSubtype.lean
@@ -0,0 +1,27 @@
+
+
+structure DecSubtype {T : Type u} (p : T → Bool) where
+mkImpl ::
+  val : T
+  property {} : p val = true
+-- #check @DecSubtype.mkImpl
+-- #check DecSubtype
+
+namespace DecSubtype
+
+variable {p : T → Bool}
+
+def mk (x : T) (h : p x = true := by rfl) : DecSubtype p :=
+⟨x,h⟩
+
+def test (t : T) : Option (DecSubtype p) :=
+if h : p t = true then
+  some ⟨t, h⟩
+else none
+
+def dtest (t : T) : DecSubtype p ⊕ PLift (p t = false) :=
+if h : p t = true then
+  Sum.inl ⟨t, h⟩
+else Sum.inr ⟨ Bool.not_eq_true _ |>.mp h ⟩
+
+end DecSubtype
